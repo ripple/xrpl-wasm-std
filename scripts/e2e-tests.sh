2e8491b620c09f9e2b11e9eeb223f3d44c5e9138
Mayukha Vadari <mvadari@gmail.com>
2025-10-08 14:35:00 -0400

    hopefully get test working in CI

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -20,21 +20,22 @@ scripts/build.sh
 scripts/build.sh release
 
 echo "🧪 Running integration tests..."
+cd tests
 if [[ "${CI:-}" == "true" || -n "${CI:-}" ]]; then
-    node tests/setup_ledger.js "wss://wasm.devnet.rippletest.net:51233"
+    node setup_ledger.js "wss://wasm.devnet.rippletest.net:51233"
 else
-    node tests/setup_ledger.js
+    node setup_ledger.js
 fi
-find examples -name "Cargo.toml" -type f | while read -r cargo_file; do
+find ../examples -name "Cargo.toml" -type f | while read -r cargo_file; do
     dir=$(dirname "$cargo_file")
     contract_name=$(basename "$dir")
-    wasm_file_release="examples/target/wasm32v1-none/release/${contract_name}.wasm"
+    wasm_file_release="../examples/target/wasm32v1-none/release/${contract_name}.wasm"
     if [[ -f "$dir/run_test.js" ]]; then
         echo "🔧 Running integration test for $contract_name in $dir"
         if [[ "${CI:-}" == "true" || -n "${CI:-}" ]]; then
-            node ./tests/run_single_test.js "$dir" "$wasm_file_release" "wss://wasm.devnet.rippletest.net:51233"
+            node ./run_single_test.js "$dir" "$wasm_file_release" "wss://wasm.devnet.rippletest.net:51233"
         else
-            node ./tests/run_single_test.js "$dir" "$wasm_file_release"
+            node ./run_single_test.js "$dir" "$wasm_file_release"
         fi
     fi
 done
