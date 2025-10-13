49c7f9725fc1d294e88effd55bbe803553936766
David Fuelling <sappenin@gmail.com>
2025-10-13 17:17:53 -0600

    Remove errant notary directives

--- examples/smart-escrows/helloworld/run_test.js
+++ examples/smart-escrows/helloworld/run_test.js
@@ -32,23 +32,6 @@ async function test(sourceWallet, destWallet, offerSequence) {
     }
 
     // Submitting EscrowFinish transaction...
-    // This should fail since the notary isn't sending this transaction
-    const responseFail = await submit(txFail, sourceWallet)
-
-    if (responseFail.result.meta.TransactionResult !== "tecWASM_REJECTED") {
-      console.log("\nEscrow finished successfully????")
-      process.exit(1)
-    }
-
-    const tx = {
-      TransactionType: "EscrowFinish",
-      Account: notary.address,
-      Owner: sourceWallet.address,
-      OfferSequence: parseInt(offerSequence),
-      ComputationAllowance: 1000000,
-    }
-
-    // Submitting EscrowFinish transaction...
     const response = await submit(tx, notary)
 
     if (response.result.meta.TransactionResult !== "tesSUCCESS") {
