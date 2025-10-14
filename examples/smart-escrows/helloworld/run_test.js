4f1015f62073e68dd3b1c5cd4022b5b1e2a00898
David Fuelling <sappenin@gmail.com>
2025-10-14 17:55:31 -0600

    Formatting

--- examples/smart-escrows/helloworld/run_test.js
+++ examples/smart-escrows/helloworld/run_test.js
@@ -1,34 +1,33 @@
-const xrpl = require("xrpl")
+const xrpl = require("xrpl");
 
 async function test(testContext) {
-    const {client, submit, sourceWallet, offerSequence} = testContext
-    try {
+  const { client, submit, sourceWallet, offerSequence } = testContext;
+  try {
+    const tx = {
+      TransactionType: "EscrowFinish",
+      Account: sourceWallet.address,
+      Owner: sourceWallet.address,
+      OfferSequence: parseInt(offerSequence),
+      ComputationAllowance: 1000000,
+    };
 
-        const tx = {
-            TransactionType: "EscrowFinish",
-            Account: sourceWallet.address,
-            Owner: sourceWallet.address,
-            OfferSequence: parseInt(offerSequence),
-            ComputationAllowance: 1000000,
-        }
+    // Submitting EscrowFinish transaction...
+    const response = await submit(tx, sourceWallet);
 
-        // Submitting EscrowFinish transaction...
-        const response = await submit(tx, sourceWallet)
-
-        if (response.result.meta.TransactionResult !== "tesSUCCESS") {
-            console.error(
-                "\nFailed to finish escrow:",
-                response.result.meta.TransactionResult,
-            )
-            process.exit(1)
-        }
-    } catch (error) {
-        console.error("Error:", error.message)
-        console.log(error)
-        process.exit(1)
-    } finally {
-        await client.disconnect()
+    if (response.result.meta.TransactionResult !== "tesSUCCESS") {
+      console.error(
+        "\nFailed to finish escrow:",
+        response.result.meta.TransactionResult,
+      );
+      process.exit(1);
     }
+  } catch (error) {
+    console.error("Error:", error.message);
+    console.log(error);
+    process.exit(1);
+  } finally {
+    await client.disconnect();
+  }
 }
 
-module.exports = {test}
+module.exports = { test };
