3bfd4218aea43185f0d66efc1491841c24b812a8
David Fuelling <sappenin@gmail.com>
2025-10-14 17:52:10 -0600

    Fix test

--- examples/smart-escrows/helloworld/run_test.js
+++ examples/smart-escrows/helloworld/run_test.js
@@ -1,49 +1,34 @@
 const xrpl = require("xrpl")
 
-// function sleep(ms) {
-//   return new Promise((resolve) => setTimeout(resolve, ms))
-// }
+async function test(testContext) {
+    const {client, submit, sourceWallet, offerSequence} = testContext
+    try {
 
-const url = process.argv.length > 4 ? process.argv[4] : "ws://127.0.0.1:6006"
-const client = new xrpl.Client(url)
+        const tx = {
+            TransactionType: "EscrowFinish",
+            Account: sourceWallet.address,
+            Owner: sourceWallet.address,
+            OfferSequence: parseInt(offerSequence),
+            ComputationAllowance: 1000000,
+        }
 
-async function submit(tx, wallet, debug = false) {
-  const result = await client.submitAndWait(tx, { autofill: true, wallet })
-  console.log("SUBMITTED " + tx.TransactionType)
-  if (debug) console.log(result.result ?? result)
-  else console.log("Result code: " + result.result?.meta?.TransactionResult)
-  return result
+        // Submitting EscrowFinish transaction...
+        const response = await submit(tx, sourceWallet)
+
+        if (response.result.meta.TransactionResult !== "tesSUCCESS") {
+            console.error(
+                "\nFailed to finish escrow:",
+                response.result.meta.TransactionResult,
+            )
+            process.exit(1)
+        }
+    } catch (error) {
+        console.error("Error:", error.message)
+        console.log(error)
+        process.exit(1)
+    } finally {
+        await client.disconnect()
+    }
 }
 
-async function test(sourceWallet, destWallet, offerSequence) {
-  try {
-    await client.connect()
-
-    const tx = {
-      TransactionType: "EscrowFinish",
-      Account: sourceWallet.address,
-      Owner: sourceWallet.address,
-      OfferSequence: parseInt(offerSequence),
-      ComputationAllowance: 1000000,
-    }
-
-    // Submitting EscrowFinish transaction...
-    const response = await submit(tx, sourceWallet)
-
-    if (response.result.meta.TransactionResult !== "tesSUCCESS") {
-      console.error(
-        "\nFailed to finish escrow:",
-        response.result.meta.TransactionResult,
-      )
-      process.exit(1)
-    }
-  } catch (error) {
-    console.error("Error:", error.message)
-    console.log(error)
-    process.exit(1)
-  } finally {
-    await client.disconnect()
-  }
-}
-
-module.exports = { test }
+module.exports = {test}
