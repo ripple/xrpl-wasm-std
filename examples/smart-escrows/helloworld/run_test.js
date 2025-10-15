cc8547803a1b2913261fe0bc7cea87546242a48b
David Fuelling <sappenin@gmail.com>
2025-10-14 18:00:35 -0600

    Remove semis

--- examples/smart-escrows/helloworld/run_test.js
+++ examples/smart-escrows/helloworld/run_test.js
@@ -1,7 +1,7 @@
-const xrpl = require("xrpl");
+const xrpl = require("xrpl")
 
 async function test(testContext) {
-  const { client, submit, sourceWallet, offerSequence } = testContext;
+  const { client, submit, sourceWallet, offerSequence } = testContext
   try {
     const tx = {
       TransactionType: "EscrowFinish",
@@ -9,25 +9,25 @@ async function test(testContext) {
       Owner: sourceWallet.address,
       OfferSequence: parseInt(offerSequence),
       ComputationAllowance: 1000000,
-    };
+    }
 
     // Submitting EscrowFinish transaction...
-    const response = await submit(tx, sourceWallet);
+    const response = await submit(tx, sourceWallet)
 
     if (response.result.meta.TransactionResult !== "tesSUCCESS") {
       console.error(
         "\nFailed to finish escrow:",
         response.result.meta.TransactionResult,
-      );
-      process.exit(1);
+      )
+      process.exit(1)
     }
   } catch (error) {
-    console.error("Error:", error.message);
-    console.log(error);
-    process.exit(1);
+    console.error("Error:", error.message)
+    console.log(error)
+    process.exit(1)
   } finally {
-    await client.disconnect();
+    await client.disconnect()
   }
 }
 
-module.exports = { test };
+module.exports = { test }
